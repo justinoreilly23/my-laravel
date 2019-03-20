02d903c78b2e8e144e5da7d791e97c09f61ffc79
Justin <jusp1tty@gmail.com>
2019-03-20 10:44:42 -0600

    clear

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,12 +10,12 @@
   <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
   <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet" >
 
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js" ></script >
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js" ></script >
+  <script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js" ></script >
+  <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js" ></script >
 
   <!-- jQuery -->
   <script rel="text/javascript"
-          src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+          src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
           crossorigin="anonymous" ></script >
 
   <!-- My CSS -->
@@ -23,7 +23,7 @@
 
   <!-- Font Awesome -->
   <link rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        href="//use.fontawesome.com/releases/v5.6.3/css/all.css"
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
         crossorigin="anonymous" >
 
@@ -54,7 +54,7 @@
 @include('partials.footer')
 
 <!-- Popper.js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
         integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
         crossorigin="anonymous" ></script >
 
